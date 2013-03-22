bd422458f6d8940fe86fd7b6baba8c594a6f6f5a
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-22 14:33:42 +0100

    Added the Research Module

---
+++ common/mod/industrialscience/modules/ResearchModules.java
@@ -0,0 +1,13 @@
+package mod.industrialscience.modules;
+
+import net.minecraft.block.Block;
+import mod.industrialscience.modules.research.*;
+
+public class ResearchModules extends ISAbstractModule {
+	public Block researchdesk;
+	public void load() {
+		
+
+	}
+
+}
--- common/mod/industrialscience/modules/research/ResearchDesk.java
+++ common/mod/industrialscience/modules/research/ResearchDesk.java
@@ -1,4 +1,4 @@
-package mod.industrialscience.research;
+package mod.industrialscience.modules.research;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
