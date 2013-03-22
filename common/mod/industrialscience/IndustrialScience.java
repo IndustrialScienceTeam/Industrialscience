eb9a48d2bb539af0614d29ab35db5c90444136f3
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-23 00:57:59 +0100

    Removed stupid import.

--- common/mod/industrialscience/IndustrialScience.java
+++ common/mod/industrialscience/IndustrialScience.java
@@ -6,7 +6,6 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import mod.industrialscience.*;
 import mod.industrialscience.modules.*;
 
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
