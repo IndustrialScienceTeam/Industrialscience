91dee2483943e777cb4cc477919e7fdd22c9e9d5
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-22 14:09:14 +0100

    Added the main mod file

---
+++ common/mod/industrialscience/IndustrialScience.java
@@ -0,0 +1,18 @@
+package mod.industrialscience;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+@Mod(modid = "industrialscience", name = "Industrial Science", version = "1.0")
+public class IndustrialScience {
+	
+	@Init
+	public void load(FMLInitializationEvent event) 
+	{
+	 
+	}
+
+}
