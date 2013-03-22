3b2d65b733fc630dfba6fd605f91c91acc98a58e
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-22 14:19:10 +0100

    Added the Block Class for the Research Desk

--- common/mod/industrialscience/IndustrialScience.java
+++ common/mod/industrialscience/IndustrialScience.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import mod.industrialscience.*;
 
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 @Mod(modid = "industrialscience", name = "Industrial Science", version = "1.0")
---
+++ common/mod/industrialscience/research/ResearchDesk.java
@@ -0,0 +1,12 @@
+package mod.industrialscience.research;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class ResearchDesk extends Block {
+
+	public ResearchDesk(int id) {
+		super(id, Material.wood);
+	}
+
+}
