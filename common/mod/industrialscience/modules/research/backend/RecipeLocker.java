956fac07b5ed244a37fa90e44004e81b29a27a3d
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-24 09:35:21 +0100

    Added the Research class.

---
+++ common/mod/industrialscience/modules/research/backend/RecipeLocker.java
@@ -0,0 +1,9 @@
+package mod.industrialscience.modules.research.backend;
+
+public interface RecipeLocker {
+
+	public void lock();
+
+	public void unlock();
+
+}
---
+++ common/mod/industrialscience/modules/research/backend/Research.java
@@ -0,0 +1,50 @@
+package mod.industrialscience.modules.research.backend;
+
+import java.util.ArrayList;
+
+public class Research implements Comparable<Research>{
+private String Name;
+private ArrayList<Research> NeededResearches;
+private String Category;
+
+public Research(String name, ArrayList<Research> neededResearches,
+		String category, Researchstep[] steps, RecipeLocker locker) {
+	Name = name;
+	NeededResearches = neededResearches;
+	Category = category;
+	Steps = steps;
+	Locker = locker;
+}
+
+private Researchstep[] Steps;
+private RecipeLocker Locker;
+
+	public int compareTo(Research o) {
+		return Name.compareTo(o.getName());
+	}
+	
+	public synchronized void unlock(){
+		Locker.unlock();
+	}
+	public synchronized void lock(){
+		Locker.lock();
+	}
+	
+
+	public String getName() {
+		return Name;
+	}
+
+	public ArrayList<Research> getNeededResearches() {
+		return NeededResearches;
+	}
+
+	public String getCategory() {
+		return Category;
+	}
+
+	public synchronized Researchstep[] getSteps() {
+		return Steps;
+	}
+
+}
