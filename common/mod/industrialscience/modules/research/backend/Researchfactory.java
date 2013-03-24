03c2ce053af5a77c6a502177756fb390723f726a
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-24 09:46:01 +0100

    Added a  Research factory

---
+++ common/mod/industrialscience/modules/research/backend/Researchfactory.java
@@ -0,0 +1,23 @@
+package mod.industrialscience.modules.research.backend;
+
+import java.util.ArrayList;
+
+public class Researchfactory{
+	private String Category;
+	private RecipeLocker Locker;
+	public Researchfactory(String category, RecipeLocker locker) {
+		super();
+		Category = category;
+		Locker = locker;
+	}
+	public Research getResearch(String Name, ArrayList<Research> NeededResearches, String Category, Researchstep[] Steps, RecipeLocker Locker){
+		return new Research(Name, NeededResearches, Category, Steps, Locker);
+	}
+	public Research getResearch(String Name, ArrayList<Research> NeededResearches, Researchstep[] Steps){
+		return new Research(Name, NeededResearches, Category, Steps, Locker);
+	}
+	public Research getResearch(String Name, ArrayList<Research> NeededResearches, Researchstep[] Steps, RecipeLocker Locker){
+		return new Research(Name, NeededResearches, Category, Steps, Locker);
+	}
+
+}
