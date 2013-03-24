278b9f67941056fc104976329c374c407072e825
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-24 01:43:05 +0100

    Added the First GUI Code
    There is a Runtime exception: class
    mod.industrialscience.modules.research.ResearchDeskTile is missing a
    mapping! This is a bug! I don't know whever this is my fault.

--- common/mod/industrialscience/modules/research/ResearchBook.java
+++ common/mod/industrialscience/modules/research/ResearchBook.java
@@ -1,9 +1,13 @@
 package mod.industrialscience.modules.research;
 
 import cpw.mods.fml.common.registry.LanguageRegistry;
+import mod.industrialscience.IndustrialScience;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ResearchBook extends Item {
 
@@ -19,5 +23,13 @@ public class ResearchBook extends Item {
 	{
 	         iconIndex = iconRegister.func_94245_a("book");
 	}
+	@Override
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World,
+			EntityPlayer par3EntityPlayer) {
+		par3EntityPlayer.openGui(IndustrialScience.instance, 1, par2World, par3EntityPlayer.getPlayerCoordinates().posX, par3EntityPlayer.getPlayerCoordinates().posY, par3EntityPlayer.getPlayerCoordinates().posZ);
+		System.out.println(par3EntityPlayer.getEntityName());
+		return super.onItemRightClick(par1ItemStack, par2World, par3EntityPlayer);
+	}
+	
 
 }
