444b834961a1d3231d7ed86777cf111f43b527d0
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-26 19:01:54 +0100

    SINGLETON!

--- common/mod/industrialscience/ResearchManager.java
+++ common/mod/industrialscience/ResearchManager.java
@@ -5,12 +5,19 @@ import java.util.ArrayList;
 import mod.industrialscience.modules.research.backend.model.Research;
 
 public class ResearchManager {
+private static ResearchManager instance=null;
 private ArrayList<Research> allResearches=null;
 private ArrayList<Research> activatedResearches=null;
 public void loadResearches(){
 	for(Research r : allResearches){
 		activatedResearches.add(r);
 	}
+}
+public static ResearchManager getInstance(){
+	if(instance==null){
+		instance= new ResearchManager();
+	}
+	return instance;
 }
 public void registerResearch(Research r){
 	
