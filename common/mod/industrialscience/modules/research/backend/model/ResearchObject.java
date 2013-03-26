9550f494d5390296408a228465e1b2db5842b5b7
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-26 21:32:54 +0100

    Added Basefunctions to the ResearchObject class

--- common/mod/industrialscience/modules/research/backend/model/ResearchObject.java
+++ common/mod/industrialscience/modules/research/backend/model/ResearchObject.java
@@ -1,5 +1,15 @@
 package mod.industrialscience.modules.research.backend.model;
 
+import net.minecraft.item.ItemStack;
+
 public class ResearchObject {
+	private ItemStack neededStack;
+	public ResearchObject(ItemStack neededStack) {
+		this.neededStack=neededStack;
+	}
+	public ItemStack getNeededStack() {
+		return neededStack;
+	}
+	
 
 }
