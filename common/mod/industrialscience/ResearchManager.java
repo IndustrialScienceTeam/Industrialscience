887c6887d42ceaaa97c9f28a01c2905c90d0e5a4
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-28 17:35:25 +0100

    Revert "Thought about it and i think sorting is dangerous"
    This reverts commit cbb42d43aef11740fd065d960a08acf0e0e9ecda.

--- common/mod/industrialscience/ResearchManager.java
+++ common/mod/industrialscience/ResearchManager.java
@@ -1,6 +1,8 @@
 package mod.industrialscience;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 import mod.industrialscience.modules.research.backend.model.Research;
 
 public class ResearchManager {
@@ -18,6 +20,7 @@ public void loadResearches(){
 		if(r.check())
 		activatedResearches.add(r);
 	}
+	Collections.sort(activatedResearches);
 }
 public static ResearchManager getInstance(){
 	if(instance==null){
