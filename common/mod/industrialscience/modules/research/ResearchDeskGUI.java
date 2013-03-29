ee626162f2092d525c5518407ec42f5fb5865ffb
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-29 12:36:20 +0100

    Changed the look of the Researchdesk GUI

--- common/mod/industrialscience/modules/research/ResearchDeskGUI.java
+++ common/mod/industrialscience/modules/research/ResearchDeskGUI.java
@@ -4,7 +4,6 @@ import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.util.StatCollector;
 
 public class ResearchDeskGUI extends GuiContainer {
     public ResearchDeskGUI(InventoryPlayer player_inventory, ResearchDeskTile tile_entity){
@@ -15,19 +14,18 @@ public class ResearchDeskGUI extends GuiContainer {
     protected void drawGuiContainerForegroundLayer(int i, int j){
    
             fontRenderer.drawString("Research Desk", 6, 6, 0xffffff);
-            fontRenderer.drawString(StatCollector.translateToLocal("container.inventory"), 6, ySize - 96 , 0xffffff);
     }
    
    
     @Override
     protected void drawGuiContainerBackgroundLayer(float f, int i, int j){
            
-            GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            this.mc.renderEngine.func_98187_b("container");
-            int x = (width - xSize) / 2;
-           
-            int y = (height - ySize) / 2;
-           
-            this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+    	GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        this.mc.renderEngine.func_98187_b("/mods/industrialscience/textures/gui/GUITemplate.png");
+        int x = (width - xSize) / 2;
+       
+        int y = (height - ySize) / 2;
+       
+        this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
     }
 }
