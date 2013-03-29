9b38732a3e332b953ccc1e595b502f5caf92f0f9
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-29 12:25:14 +0100

    Updated the cordinates

--- common/mod/industrialscience/ISContainer.java
+++ common/mod/industrialscience/ISContainer.java
@@ -11,13 +11,13 @@ protected ISContainer(InventoryPlayer ip){
 private void bindPlayerInventory(InventoryPlayer ip) {
 	for(int i = 0; i < 3; i++){
         for(int j = 0; j < 9; j++){
-                addSlotToContainer(new Slot(ip, j + i * 9 + 9, 9 + j * 18, 85 + i * 16));
+                addSlotToContainer(new Slot(ip, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
                
         }
 }
 
 for(int i = 0; i < 9; i++){
-        addSlotToContainer(new Slot(ip, i, 6 + i * 16, 84));
+        addSlotToContainer(new Slot(ip, i, 8 + (i * 18), 142));
 }
 	
 }
