2b7380a79f67dab1d18ab8842979b8a7a7a9c524
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-30 14:17:50 +0100

    Added a block for copying researches

---
+++ common/mod/industrialscience/modules/research/Copier.java
@@ -0,0 +1,21 @@
+package mod.industrialscience.modules.research;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class Copier extends BlockContainer {
+
+	protected Copier(int par1) {
+		super(par1, Material.wood);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
