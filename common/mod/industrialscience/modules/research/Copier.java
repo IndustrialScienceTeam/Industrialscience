cc957416e5a77756bb9b8ab17f82327edd00b54a
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-03-30 14:23:00 +0100

    Copied and pasted the code from the researchdesk

--- common/mod/industrialscience/modules/research/Copier.java
+++ common/mod/industrialscience/modules/research/Copier.java
@@ -2,20 +2,47 @@ package mod.industrialscience.modules.research;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
 public class Copier extends BlockContainer {
-
-	protected Copier(int par1) {
-		super(par1, Material.wood);
-		// TODO Auto-generated constructor stub
+	private Icon side;
+	private Icon bottom;
+	private Icon top;
+	protected Copier(int id) {
+		super(id, Material.wood);
+	}
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+	@Override 
+	public void func_94332_a(IconRegister par1IconRegister)
+	{
+	this.side= par1IconRegister.func_94245_a("industrialscience:vannila_researchtable_sides");
+	this.bottom = par1IconRegister.func_94245_a("industrialscience:vannila_researchtable_bottom"); 
+	this.top = par1IconRegister.func_94245_a("wood");
 	}
 
+	public Icon getBlockTextureFromSideAndMetadata(int i, int j){
+		switch (i) {
+		case 0:
+			return bottom;
+		case 1:
+			return top;
+		default:
+			return side;
+			}
+		}
 	@Override
 	public TileEntity createNewTileEntity(World world) {
-		// TODO Auto-generated method stub
 		return null;
+	}
+	public boolean hasTileEntity(int metadata)
+	{
+	    return true;
 	}
 
 }
