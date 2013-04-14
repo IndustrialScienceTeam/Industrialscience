11f28e3cf847d8ae574f5b75b24dfa05428b479b
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-04-14 16:30:39 +0200

    Added a drop method

--- common/mod/industrialscience/modules/fishing/FishtrapBlock.java
+++ common/mod/industrialscience/modules/fishing/FishtrapBlock.java
@@ -41,5 +41,42 @@ public class FishtrapBlock extends BlockContainer {
 	public void updateTick(World world, int x, int y, int z, Random random){
 		
 	}
+    public void breakBlock(World world, int x, int y, int z, int i, int j){
+        dropItems(world, x, y, z);
+        super.breakBlock(world, x, y, z, i, j);
+        }
+    private void dropItems(World world, int x, int y, int z){
+        Random rand = new Random();
+       
+        TileEntity tile_entity = world.getBlockTileEntity(x, y, z);
+       
+        if(!(tile_entity instanceof IInventory)){
+                return;
+        }
+
+        IInventory inventory = (IInventory) tile_entity;
+
+        for(int i = 0; i < inventory.getSizeInventory(); i++){
+                ItemStack item = inventory.getStackInSlot(i);
+               
+                if(item != null && item.stackSize > 0){
+                float rz = rand.nextFloat() * 0.3F + 0.1F;
+               
+                EntityItem entity_item = new EntityItem(world, x, y , z + rz, new ItemStack(item.itemID, item.stackSize, item.getItemDamage()));
+               
+                if(item.hasTagCompound()){
+                        entity_item.writeToNBT(((NBTTagCompound) item.getTagCompound().copy()));
+                }
+
+                float factor = 0.5F;
+               
+                entity_item.motionX = rand.nextGaussian() * factor;
+                entity_item.motionY = rand.nextGaussian() * factor + 0.2F;
+                entity_item.motionZ = rand.nextGaussian() * factor;
+                world.spawnEntityInWorld(entity_item);
+                item.stackSize = 0;
+                }
+                }
+        }
 
 }
