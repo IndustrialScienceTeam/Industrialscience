c715c68aba64039b252249d8599316eee78a1166
Marc André Eckhoff <MAESchortens@web.de>
2013-04-20 22:01:48 +0200

    Added obster trap

--- common/mod/industrialscience/modules/FishingModule.java
+++ common/mod/industrialscience/modules/FishingModule.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 import mod.industrialscience.modules.fishing.FishtrapBlock;
+import mod.industrialscience.modules.fishing.Lobster_trapBlock;
 import mod.industrialscience.modules.fishing.Fishtraptile;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -15,15 +16,23 @@ public class FishingModule extends ISAbstractModule {
 	public Block basicfishtrap;
 	public int basicfishtrapid;
 
+	public Block lobster_trap;
+	public int lobster_trapid;
 	@Override
 	public void load() {
 		logger.log(Level.INFO, "LOADING");
+		
 		//Basicfishtrap
 		GameRegistry.registerBlock(basicfishtrap,basicfishtrap.getUnlocalizedName());
 		GameRegistry.registerTileEntity(Fishtraptile.class,"Basic-Fishtrap");
 		basicfishtrap.setCreativeTab(CreativeTab);
 		GameRegistry.addRecipe(new ItemStack(basicfishtrap),new Object[]{"X X X"," X X ","X X X", Character.valueOf('X'), Item.stick});
 		
+		//Lobster trap
+		GameRegistry.registerBlock(lobster_trap,lobster_trap.getUnlocalizedName());
+		GameRegistry.registerTileEntity(Fishtraptile.class,"Lobster-Trap");
+		lobster_trap.setCreativeTab(CreativeTab);
+		GameRegistry.addRecipe(new ItemStack(basicfishtrap),new Object[]{"X X X"," X X ","X X X", Character.valueOf('X'), Item.paper});
 
 	}
 
@@ -31,15 +40,23 @@ public class FishingModule extends ISAbstractModule {
 	public void init() {
 		initCreativeTab();
 		logger.log(Level.INFO, "INIT");
+		
+		//Basicfishtrap
 		basicfishtrapid=BlockIDs.get("basicfishtrap");
 		basicfishtrap=new FishtrapBlock(basicfishtrapid);
-
+		
+		//Lobster trap
+		lobster_trapid=BlockIDs.get("lobster_trap");
+		lobster_trap=new Lobster_trapBlock(lobster_trapid);
 	}
 
 	@Override
 	public Hashtable<String, Integer> getNeededBlockIDs() {
 		Hashtable<String, Integer> NeededBlockIDs = new Hashtable<String, Integer>();
-		NeededBlockIDs.put("basicfishtrap", 758);
+		
+		NeededBlockIDs.put("basicfishtrap", 758); //Basicfishtrap
+		NeededBlockIDs.put("lobster_trap", 759); //Lobster trap
+		
 		return NeededBlockIDs;
 	}
 
---
+++ common/mod/industrialscience/modules/fishing/Lobster_trapBlock.java
@@ -0,0 +1,103 @@
+package mod.industrialscience.modules.fishing;
+
+import java.util.Random;
+
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
+import net.minecraft.world.World;
+
+public class Lobster_trapBlock extends BlockContainer {
+	protected Icon texture;
+	protected final Random random = new Random();
+	protected int fishamout=1;
+	protected int neededwater=4;
+	protected int waterforextrafish=8;
+	protected int range=5;
+	protected static Material material=Material.wood;
+	public Lobster_trapBlock(int id) {
+		super(id, material);
+		setUnlocalizedName("Lobster trap");
+		LanguageRegistry.addName(this, "Lobster trap");
+		setTickRandomly(true);
+	}
+	@Override
+	public TileEntity createNewTileEntity(World world) {
+		return new Lobster_traptile();
+	}
+	public boolean hasTileEntity(int metadata)
+	{
+	    return true;
+	}
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+	public void registerIcons(IconRegister par1IconRegister)
+	{
+	this.texture= par1IconRegister.registerIcon("industrialscience:lobstertrap");
+	}
+	public Icon getBlockTextureFromSideAndMetadata(int i, int j){
+		return texture;
+		}
+	public void updateTick(World world, int x, int y, int z, Random random){
+			TileEntity tile =world.getBlockTileEntity(x, y, z);
+	        if(!(tile instanceof Fishtraptile)){
+                return;
+	        }
+	        Fishtraptile fishtile = (Fishtraptile) tile;
+			fishtile.addFish(fishamout, neededwater, range,waterforextrafish);
+		
+	}
+    public void breakBlock(World world, int x, int y, int z, int i, int j){
+        dropItems(world, x, y, z);
+        super.breakBlock(world, x, y, z, i, j);
+        }
+    protected void dropItems(World par1World, int x, int y, int z){
+    	TileEntity tileentity = par1World.getBlockTileEntity(x, y, z);
+    	if(tileentity instanceof IInventory){
+    		IInventory INV = (IInventory) tileentity;
+            for (int j1 = 0; j1 < INV.getSizeInventory(); ++j1)
+            {
+                ItemStack itemstack = INV.getStackInSlot(j1);
+
+                if (itemstack != null)
+                {
+                    float f = this.random.nextFloat() * 0.8F + 0.1F;
+                    float f1 = this.random.nextFloat() * 0.8F + 0.1F;
+                    EntityItem entityitem;
+
+                    for (float f2 = this.random.nextFloat() * 0.8F + 0.1F; itemstack.stackSize > 0; par1World.spawnEntityInWorld(entityitem))
+                    {
+                        int k1 = this.random.nextInt(21) + 10;
+
+                        if (k1 > itemstack.stackSize)
+                        {
+                            k1 = itemstack.stackSize;
+                        }
+
+                        itemstack.stackSize -= k1;
+                        entityitem = new EntityItem(par1World, (double)((float)x + f), (double)((float)y + f1), (double)((float)z + f2), new ItemStack(itemstack.itemID, k1, itemstack.getItemDamage()));
+                        float f3 = 0.05F;
+                        entityitem.motionX = (double)((float)this.random.nextGaussian() * f3);
+                        entityitem.motionY = (double)((float)this.random.nextGaussian() * f3 + 0.5F);
+                        entityitem.motionZ = (double)((float)this.random.nextGaussian() * f3);
+
+                        if (itemstack.hasTagCompound())
+                        {
+                            entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
+                        }
+                    }
+                }
+                }
+            
+            }
+        }
+}
