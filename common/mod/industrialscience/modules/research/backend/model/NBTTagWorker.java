07613e17375f2cec20bda0d64a5eac7163fa6963
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-04-21 08:10:55 +0200

    Added a NBT woker to get the reading and writing into one class

---
+++ common/mod/industrialscience/modules/research/backend/model/NBTTagWorker.java
@@ -0,0 +1,13 @@
+package mod.industrialscience.modules.research.backend.model;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public class NBTTagWorker {
+public static void writeResearchesToNBT(NBTTagCompound compund){
+	
+}
+public static void readResearchesfromNBT(NBTTagCompound compund){
+	
+}
+
+}
