8b3b9c396a64580cfd8b1ad9109136b8abe51d5d
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-05-02 17:48:54 +0200

    Added first classes

---
+++ common/mod/industrialscience/modules/fishing/TrapCraftingTableContainer.java
@@ -0,0 +1,11 @@
+package mod.industrialscience.modules.fishing;
+import net.minecraft.entity.player.InventoryPlayer;
+import mod.industrialscience.ISContainer;
+
+public class TrapCraftingTableContainer extends ISContainer {
+
+	public TrapCraftingTableContainer(TrapCraftingTableTile tile,InventoryPlayer ip) {
+		super(ip, tile);
+	}
+
+}
