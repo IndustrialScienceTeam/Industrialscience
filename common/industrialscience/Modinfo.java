55d99f785460fd70864f2bfbac67f7f7ce17f8d3
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-08-06 23:16:28 +0200

    Renamed channels

--- common/industrialscience/Modinfo.java
+++ common/industrialscience/Modinfo.java
@@ -2,6 +2,7 @@ package industrialscience;
 
 public class Modinfo {
 public final static String ReadableModName="Industrial Science";
-public final static String ModID="industrial_science";
+public final static String ModID="industrialscience";
+public final static String channelbase="indussci";
 public final static String Version="1.0";
 }
