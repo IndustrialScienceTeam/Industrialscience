bad549e68924cfd07115c349c0d792de13a950d1
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-11-21 12:27:27 +0100

    Now using the dropItems method everywhere
    Thx to SonarQube

--- common/industrialscience/modules/fishing/FishingBlock.java
+++ common/industrialscience/modules/fishing/FishingBlock.java
@@ -1,5 +1,7 @@
 package industrialscience.modules.fishing;
 
+import industrialscience.BlockUtils;
+
 import java.util.List;
 import java.util.Random;
 
@@ -7,11 +9,8 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
@@ -73,50 +72,11 @@ public class FishingBlock extends BlockContainer {
 
     }
 
-    private void dropItems(World world, int x, int y, int z) {
-        Random rand = new Random();
-
-        TileEntity tile_entity = world.getBlockTileEntity(x, y, z);
-
-        if (!(tile_entity instanceof IInventory))
-            return;
-
-        IInventory inventory = (IInventory) tile_entity;
-
-        for (int i = 0; i < inventory.getSizeInventory(); i++) {
-            ItemStack item = inventory.getStackInSlot(i);
-
-            if (item != null && item.stackSize > 0) {
-                float rx = rand.nextFloat() * 0.3F + 0.1F;
-                float ry = rand.nextFloat() * 0.3F + 0.1F;
-                float rz = rand.nextFloat() * 0.3F + 0.1F;
-
-                EntityItem entity_item = new EntityItem(world, x + rx, y + ry,
-                        z + rz, new ItemStack(item.itemID, item.stackSize,
-                                item.getItemDamage()));
-
-                if (item.hasTagCompound()) {
-                    entity_item.writeToNBT((NBTTagCompound) item
-                            .getTagCompound().copy());
-                }
-
-                float factor = 0.5F;
-
-                entity_item.motionX = rand.nextGaussian() * factor;
-                entity_item.motionY = rand.nextGaussian() * factor;
-                entity_item.motionZ = rand.nextGaussian() * factor;
-                world.spawnEntityInWorld(entity_item);
-                item.stackSize = 0;
-            }
-        }
-
-    }
-
     @Override
     public void breakBlock(World world, int x, int y, int z, int i, int j) {
         FishingBlockType.values()[world.getBlockMetadata(x, y, z)].breakBlock(
                 world, x, y, z, i, j);
-        dropItems(world, x, y, z);
+        BlockUtils.dropItems(world, x, y, z);
         super.breakBlock(world, x, y, z, i, j);
     }
 
--- common/industrialscience/modules/fishing/TrapCraftingTable.java
+++ common/industrialscience/modules/fishing/TrapCraftingTable.java
@@ -1,5 +1,7 @@
 package industrialscience.modules.fishing;
 
+import industrialscience.BlockUtils;
+
 import java.util.Random;
 
 import net.minecraft.block.BlockContainer;
@@ -49,47 +51,10 @@ public class TrapCraftingTable extends BlockContainer {
 
     @Override
     public void breakBlock(World world, int x, int y, int z, int i, int j) {
-        dropItems(world, x, y, z);
+        BlockUtils.dropItems(world, x, y, z);
         super.breakBlock(world, x, y, z, i, j);
     }
 
-    private void dropItems(World world, int x, int y, int z) {
-        Random rand = new Random();
-
-        TileEntity tile_entity = world.getBlockTileEntity(x, y, z);
-
-        if (!(tile_entity instanceof IInventory))
-            return;
-
-        IInventory inventory = (IInventory) tile_entity;
-
-        for (int i = 0; i < inventory.getSizeInventory(); i++) {
-            ItemStack item = inventory.getStackInSlot(i);
-
-            if (item != null && item.stackSize > 0) {
-                float rx = rand.nextFloat() * 0.3F + 0.1F;
-                float ry = rand.nextFloat() * 0.3F + 0.1F;
-                float rz = rand.nextFloat() * 0.3F + 0.1F;
-
-                EntityItem entity_item = new EntityItem(world, x + rx, y + ry,
-                        z + rz, new ItemStack(item.itemID, item.stackSize,
-                                item.getItemDamage()));
-
-                if (item.hasTagCompound()) {
-                    entity_item.writeToNBT((NBTTagCompound) item
-                            .getTagCompound().copy());
-                }
-
-                float factor = 0.5F;
-
-                entity_item.motionX = rand.nextGaussian() * factor;
-                entity_item.motionY = rand.nextGaussian() * factor;
-                entity_item.motionZ = rand.nextGaussian() * factor;
-                world.spawnEntityInWorld(entity_item);
-                item.stackSize = 0;
-            }
-        }
-    }
 
     @Override
     public TileEntity createNewTileEntity(World world) {
