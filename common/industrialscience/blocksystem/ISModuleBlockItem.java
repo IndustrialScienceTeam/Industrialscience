d6500db3d25c7eca1fd403ea338d1e1c91123afb
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-11-22 21:24:13 +0100

    Fixed the naming bug

--- common/industrialscience/blocksystem/ISModuleBlockItem.java
+++ common/industrialscience/blocksystem/ISModuleBlockItem.java
@@ -1,12 +1,26 @@
 package industrialscience.blocksystem;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 
 public class ISModuleBlockItem extends ItemBlock{
 
 	public ISModuleBlockItem(int id) {
 		super(id);
 		setHasSubtypes(true);
+	}
+	@Override
+	public int getMetadata (int damageValue) {
+		return damageValue;
+	}
+	@Override
+	public String getUnlocalizedName(ItemStack itemstack) {
+		if(Block.blocksList[itemstack.itemID] instanceof ISModuleBlock){
+			ISModuleBlock mBlock=(ISModuleBlock)Block.blocksList[itemstack.itemID];
+			return mBlock.getIDName(itemstack.getItemDamage());
+		}
+		return "NO NAME";
 	}
 	
 	
