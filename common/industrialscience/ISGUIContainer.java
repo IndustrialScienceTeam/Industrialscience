91fed3e0b1909808fb8e32aed5631d85d947b7be
Lukas Jansen <lukas.jansen1997@yahoo.de>
2013-11-26 17:21:54 +0100

    Updated the GUI API.

--- common/industrialscience/ISGUIContainer.java
+++ common/industrialscience/ISGUIContainer.java
@@ -11,14 +11,17 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class ISGUIContainer extends GuiContainer {
-    ResourceLocation texture;
+public abstract  class ISGUIContainer extends GuiContainer {
+    protected ResourceLocation texture;
+    protected String title;
+    
 
-    public ISGUIContainer(Container par1Container, ResourceLocation rl) {
+    public ISGUIContainer(Container par1Container, ResourceLocation rl, String title) {
         super(par1Container);
         xSize = 182;
         ySize = 171;
         texture = rl;
+        this.title=title;
     }
 
     @Override
@@ -26,6 +29,17 @@ public class ISGUIContainer extends GuiContainer {
         GL11.glColor4f(1, 1, 1, 1);
         Minecraft.getMinecraft().renderEngine.bindTexture(texture);
         drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+        _drawGuiContainerBackgroundLayer(f, i, j);
+        
+        
     }
+    @Override
+    protected void drawGuiContainerForegroundLayer(int par1, int par2) {
+        fontRenderer.drawString(title, 8, 6, 0x404040);
+    }
+    protected abstract void _drawGuiContainerBackgroundLayer(float f, int i, int j);
+    protected abstract void _drawGuiContainerForegroundLayer(int par1, int par2);
+    
+    
 
 }
