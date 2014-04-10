696148c9db7600d1ba262f18c519569332584c6d
Finesim97 <lukas.jansen1997@yahoo.de>
2014-04-11 00:41:34 +0200

    Just added a material, to remove an exception.

--- src/main/java/de/zsgn/industrialscience/blocks/SingularityConverter.java
+++ src/main/java/de/zsgn/industrialscience/blocks/SingularityConverter.java
@@ -1,7 +1,12 @@
 package de.zsgn.industrialscience.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 public class SingularityConverter extends Block {
+
+	protected SingularityConverter() {
+		super(Material.iron);
+	}
 
 }
