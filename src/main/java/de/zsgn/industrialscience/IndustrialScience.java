25ace30a1d39906466045b01a6a8a1833958c9ef
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-04-17 09:16:49 +0200

    Added a command, that prints the current version.
    /is version

--- src/main/java/de/zsgn/industrialscience/IndustrialScience.java
+++ src/main/java/de/zsgn/industrialscience/IndustrialScience.java
@@ -4,16 +4,20 @@ import org.apache.logging.log4j.Level;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.command.ServerCommandManager;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import de.zsgn.industrialscience.blocks.BlockSingularity;
+import de.zsgn.industrialscience.commands.IndustrialScienceMainCommand;
 import de.zsgn.industrialscience.items.ItemAncientTechnology;
 
 @Mod(modid = IndustrialScience.MODID, version = IndustrialScience.VERSION)
@@ -28,7 +32,7 @@ public class IndustrialScience
     
     public static CreativeTabs creativetab = new IndustrialScienceCreativeTab();
     
-    
+    public final IndustrialScienceMainCommand industrialScienceMainCommand=new IndustrialScienceMainCommand();
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
@@ -44,9 +48,17 @@ public class IndustrialScience
         
     }
 
-
 	private void addRecipes() {
 		GameRegistry.addSmelting(Items.diamond, new ItemStack(blocksingularity), 9001);
 		GameRegistry.addRecipe(new ItemStack(blocksingularity), " X ", " X ","BBB",'X',Items.apple,'B', Items.book);
+	}
+	
+	@EventHandler
+	public void serverStart(FMLServerStartingEvent event)
+	{
+	         MinecraftServer server = MinecraftServer.getServer();
+	         ServerCommandManager manager = (ServerCommandManager) server.getCommandManager();
+	         manager.registerCommand(industrialScienceMainCommand);
+	         
 	}
 }
