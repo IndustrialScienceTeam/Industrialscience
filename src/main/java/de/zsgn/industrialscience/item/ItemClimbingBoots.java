c2a5132c4c3cd9c865eb70a28dc750a81a87bb4e
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-05-24 15:45:23 +0200

    Now using the icon string variable

--- src/main/java/de/zsgn/industrialscience/item/ItemClimbingBoots.java
+++ src/main/java/de/zsgn/industrialscience/item/ItemClimbingBoots.java
@@ -9,22 +9,15 @@ public class ItemClimbingBoots extends ItemArmor{
 	
 	public ItemClimbingBoots(){
 		
-	//first argument is the material of this armor , the second one is unknown , according to ItemArmor.class it´s the renderIndex , I recommend using 0 , because it is working, the last one is the slot (0 = helmet , 1 = chestplate , 2 = leggings , 3 = boots)	
+	//first argument is the material of this armor , the second one is unknown , according to ItemArmor.class itï¿½s the renderIndex , I recommend using 0 , because it is working, the last one is the slot (0 = helmet , 1 = chestplate , 2 = leggings , 3 = boots)	
 	super(ArmorMaterial.CLOTH,0,3);
 	
 	setCreativeTab(IndustrialScience.creativetab);
 	
 	setUnlocalizedName("climbingboots");
 	
-	
+	iconString =IndustrialScience.MODID + ":" + this.getUnlocalizedName().substring(5);
 	}
-	
-	/*
-	@Override
-	public void registerIcons(IIconRegister par1IconRegister) {
-		itemIcon = par1IconRegister.registerIcon(IndustrialScience.MODID + ":" + this.getUnlocalizedName().substring(5));
-	}
-	*/
 }
 
 
--- src/main/java/de/zsgn/industrialscience/item/ItemCrack.java
+++ src/main/java/de/zsgn/industrialscience/item/ItemCrack.java
@@ -21,6 +21,9 @@ public class ItemCrack extends ItemFood {
 		//required for the naming stuff (this is not the display name)
 		setUnlocalizedName("Crack");
 		
+		//sets the texture path
+		iconString =IndustrialScience.MODID + ":" + this.getUnlocalizedName().substring(5);
+		
 		//makes sure, that you can eat crack all day
 		setAlwaysEdible();
 	}
@@ -36,11 +39,6 @@ public class ItemCrack extends ItemFood {
 			//Adds Jump Boost (8) for 10 seconds (200 ticks) and increases its level by 1 (1): Jump boost 2 for 10 seconds
 			par3EntityPlayer.addPotionEffect(new PotionEffect(8, 200, 1));
 		}
-	}
-
-	@Override
-	public void registerIcons(IIconRegister par1IconRegister) {
-		itemIcon = par1IconRegister.registerIcon(IndustrialScience.MODID + ":" + this.getUnlocalizedName().substring(5));
 	}
 
 }
