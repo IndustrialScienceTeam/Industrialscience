0d06ba0178ae1781c5a7d2c47505a744c4e1ad4a
Marc André Eckhoff <MAESchortens@web.de>
2014-05-26 21:38:19 +0200

    It´s rendering! Looks stupid!
    If you equip the climbing boots , you will have a colorful plate
    hovering under youre feets, but it isn´t crashing anymore.

--- src/main/java/de/zsgn/industrialscience/item/ItemClimbingBoots.java
+++ src/main/java/de/zsgn/industrialscience/item/ItemClimbingBoots.java
@@ -1,6 +1,11 @@
 package de.zsgn.industrialscience.item;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import de.zsgn.industrialscience.IndustrialScience;
@@ -24,5 +29,46 @@ public class ItemClimbingBoots extends ItemArmor{
             return IndustrialScience.MODID + ":" + "textures/items/" + this.getUnlocalizedName().substring(5)+"_model.png";
     }
     
+    @Override
+    @SideOnly(Side.CLIENT)
+    public ModelBiped getArmorModel(EntityLivingBase entityLiving,
+    ItemStack itemStack, int armorSlot) {
+        
+        ModelBiped armorModel = null;
+        if(itemStack != null){
+            if(itemStack.getItem() instanceof ItemClimbingBoots){
+                 climbingboots_model climbingboots = new climbingboots_model(1.0f);
+                armorModel = climbingboots;
+            }
+            
+            
+            
+        }
+        if(armorModel != null){
+            armorModel.bipedHead.showModel = armorSlot == 0;
+            armorModel.bipedHeadwear.showModel = armorSlot == 0;
+            armorModel.bipedBody.showModel = armorSlot == 1 || armorSlot == 2;
+            armorModel.bipedRightArm.showModel = armorSlot == 1;
+            armorModel.bipedLeftArm.showModel = armorSlot == 1;
+            armorModel.bipedRightLeg.showModel = armorSlot == 2 || armorSlot == 3;
+            armorModel.bipedLeftLeg.showModel = armorSlot == 2 || armorSlot == 3;
+
+            armorModel.isSneak = entityLiving.isSneaking();
+            armorModel.isRiding = entityLiving.isRiding();
+            armorModel.isChild = entityLiving.isChild();
+            //armorModel.heldItemRight = entityLiving.getCurrentItemOrArmor(0) != null ? 1 :0;
+            if(entityLiving instanceof EntityPlayer){
+            armorModel.aimedBow =((EntityPlayer)entityLiving).getItemInUseDuration() > 2;
+            }
+            return armorModel;
+            }
+            
+
+            return null;
+            }
+            
     
-}
+    }
+    
+    
+
