5251d0d9fa852551baf40789eaef8b0508a49a8d
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-05-26 15:01:33 +0200

    Remove every Entity, that walks into the portal room.
    Doesn’t work with portal rooms and you have to activate the portal
    block with an rs torch

--- src/main/java/de/zsgn/industrialscience/tileentity/TileEntityMysteriousPortal.java
+++ src/main/java/de/zsgn/industrialscience/tileentity/TileEntityMysteriousPortal.java
@@ -1,8 +1,13 @@
 package de.zsgn.industrialscience.tileentity;
 
+import java.util.Iterator;
+import java.util.List;
+
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
@@ -25,10 +30,24 @@ public class TileEntityMysteriousPortal extends TileEntity {
         return false;
     }
     
+    @SuppressWarnings("rawtypes")
     @Override
     public void updateEntity() {
         if(isUseable()){
                     setActive(worldObj.getIndirectPowerLevelTo(xCoord, yCoord, zCoord, 0)>0);
+                    if(isActive()){
+                        AxisAlignedBB axisalignedbb =  AxisAlignedBB.getAABBPool().getAABB((double)(this.xCoord - 1), (double)(this.yCoord+1), (double)(this.zCoord - 1), (double)(this.xCoord + 1), (double)(this.yCoord + 3), (double)(this.zCoord + 1));
+                        List list=  this.worldObj.getEntitiesWithinAABB(Entity.class, axisalignedbb);
+                        Iterator iterator = list.iterator();
+                        while (iterator.hasNext()) {
+                            Object object = iterator.next();
+                            if(object instanceof Entity){
+                                Entity entity =(Entity) object;
+                                entity.setDead();
+                            }
+                            
+                        }
+                    }
         }
     }
 
