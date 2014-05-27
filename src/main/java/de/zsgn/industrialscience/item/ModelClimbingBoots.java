8d302d24b0d8ba1aec6f36633397b4eeda8d8afa
Marc André Eckhoff <MAESchortens@web.de>
2014-05-27 22:46:58 +0200

    Did some cleanup too , renamed the two shapes to match name convention.

--- src/main/java/de/zsgn/industrialscience/item/ModelClimbingBoots.java
+++ src/main/java/de/zsgn/industrialscience/item/ModelClimbingBoots.java
@@ -4,66 +4,52 @@
 // Keep in mind that you still need to fill in some blanks
 // - ZeuX
 
-
-
-
-
-
 package de.zsgn.industrialscience.item;
 
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelClimbingBoots extends ModelBiped
-{
-  //fields
-    ModelRenderer Shape1;
-    ModelRenderer Shape2;
-
-  
-  public ModelClimbingBoots(float f)
-  {
-      super(f);
-    textureWidth = 64;
-    textureHeight = 64;
+public class ModelClimbingBoots extends ModelBiped {
+    // fields
+    ModelRenderer shoe;
+    ModelRenderer spike;
     
-      Shape1 = new ModelRenderer(this, 0, 0);
-      Shape1.addBox(0F, 0F, 0F, 10, 3, 6);
-      Shape1.setRotationPoint(-5F, 21F, -3F);
-      Shape1.setTextureSize(64, 32);
-      Shape1.mirror = true;
-      setRotation(Shape1, 0F, 0F, 0F);
-      Shape2 = new ModelRenderer(this, 0, 0);
-      Shape2.addBox(0F, 0F, 0F, 1, 2, 2);
-      Shape2.setRotationPoint(2F, 21F, -5F);
-      Shape2.setTextureSize(64, 32);
-      Shape2.mirror = true;
-      setRotation(Shape2, 0F, 0F, 0F);
-      
-      this.bipedLeftLeg.addChild(Shape1);
-      this.bipedRightLeg.addChild(Shape2);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    Shape1.render(f5);
-    Shape2.render(f5);
+    public ModelClimbingBoots(float f) {
+        super(f);
+        textureWidth = 64;
+        textureHeight = 64;
+        
+        shoe = new ModelRenderer(this, 0, 0);
+        shoe.addBox(0F, 0F, 0F, 10, 3, 6);
+        shoe.setRotationPoint(-5F, 21F, -3F);
+        shoe.setTextureSize(64, 32);
+        shoe.mirror = true;
+        setRotation(shoe, 0F, 0F, 0F);
+        spike = new ModelRenderer(this, 0, 0);
+        spike.addBox(0F, 0F, 0F, 1, 2, 2);
+        spike.setRotationPoint(2F, 21F, -5F);
+        spike.setTextureSize(64, 32);
+        spike.mirror = true;
+        setRotation(spike, 0F, 0F, 0F);
+        
+        this.bipedLeftLeg.addChild(shoe);
+        this.bipedRightLeg.addChild(spike);
+    }
     
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  /*public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5);
-  }*/
-
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3,
+            float f4, float f5) {
+        super.render(entity, f, f1, f2, f3, f4, f5);
+        super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        shoe.render(f5);
+        spike.render(f5);
+        
+    }
+    
+    private void setRotation(ModelRenderer model, float x, float y, float z) {
+        model.rotateAngleX = x;
+        model.rotateAngleY = y;
+        model.rotateAngleZ = z;
+    }
 }
