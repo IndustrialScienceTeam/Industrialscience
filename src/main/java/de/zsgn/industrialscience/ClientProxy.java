c096ec60fb15e66219266f9c378fa10a1a07bb7a
Marc André Eckhoff <MAESchortens@web.de>
2014-05-27 22:54:04 +0200

    And another cleanup and some name changes

--- src/main/java/de/zsgn/industrialscience/ClientProxy.java
+++ src/main/java/de/zsgn/industrialscience/ClientProxy.java
@@ -1,24 +1,14 @@
 package de.zsgn.industrialscience;
 
-import de.zsgn.industrialscience.item.ModelClimbingBoots;
 import net.minecraft.client.model.ModelBiped;
+import de.zsgn.industrialscience.item.ModelClimbingBoots;
 
 public class ClientProxy extends CommonProxy {
-
-    private static final ModelClimbingBoots normal = new ModelClimbingBoots(1.0f);
-    private static final ModelClimbingBoots smaller = new ModelClimbingBoots(0.5f);
     
-    public ModelBiped getArmorModel(int id){
-        switch (id) {
-        case 0:
-        return normal;
-        case 1:
-        return normal;
-        default:
-        break;
-        }
-        return normal; 
-        }
-        
-
+    @Override
+    public ModelBiped getArmorModel(int id) {
+        ModelClimbingBoots modelClimbingBoots = new ModelClimbingBoots(1.0f);
+        return modelClimbingBoots;
+    }
+    
 }
