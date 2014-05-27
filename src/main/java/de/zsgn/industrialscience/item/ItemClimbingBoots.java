442efbe98247134a80125cccfc8bb4701ba65b8d
Marc André Eckhoff <MAESchortens@web.de>
2014-05-27 22:38:17 +0200

    Did some cleanup

--- src/main/java/de/zsgn/industrialscience/item/ItemClimbingBoots.java
+++ src/main/java/de/zsgn/industrialscience/item/ItemClimbingBoots.java
@@ -1,71 +1,76 @@
 package de.zsgn.industrialscience.item;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import de.zsgn.industrialscience.IndustrialScience;
 
-public class ItemClimbingBoots extends ItemArmor{
+public class ItemClimbingBoots extends ItemArmor {
     
-    public ItemClimbingBoots(){
+    public ItemClimbingBoots() {
         
-        //first argument is the material of this armor , the second one is unknown , according to ItemArmor.class it is the renderIndex , I recommend using 0 , because it is working, the last one is the slot (0 = helmet , 1 = chestplate , 2 = leggings , 3 = boots)	
-        super(ArmorMaterial.IRON,0,3);
+        // first argument is the material of this armor , the second one is
+        // unknown , according to ItemArmor.class it is the renderIndex , I
+        // recommend using 0 , because it is working, the last one is the slot
+        // (0 = helmet , 1 = chestplate , 2 = leggings , 3 = boots)
+        super(ArmorMaterial.IRON, 0, 3);
         
         setCreativeTab(IndustrialScience.getInstance().getCreativetab());
         
         setUnlocalizedName("climbingboots");
         
-        iconString =IndustrialScience.MODID + ":" + this.getUnlocalizedName().substring(5);
+        iconString = IndustrialScience.MODID + ":"
+                + this.getUnlocalizedName().substring(5);
     }
     
     @Override
-    public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type) {
-            return IndustrialScience.MODID + ":" + "textures/items/" + this.getUnlocalizedName().substring(5)+"_model.png";
+    public String getArmorTexture(ItemStack stack, Entity entity, int slot,
+            String type) {
+        return IndustrialScience.MODID + ":" + "textures/items/"
+                + this.getUnlocalizedName().substring(5) + "_model.png";
     }
     
     @Override
     @SideOnly(Side.CLIENT)
     public ModelBiped getArmorModel(EntityLivingBase entityLiving,
-    ItemStack itemStack, int armorSlot) {
+            ItemStack itemStack, int armorSlot) {
         
         ModelBiped armorModel = null;
         
-        if(itemStack != null && itemStack.getItem() instanceof ItemClimbingBoots){           
-                armorModel = IndustrialScience.proxy.getArmorModel(0);
-            }
-   
+        if (itemStack != null
+                && itemStack.getItem() instanceof ItemClimbingBoots) {
+            armorModel = IndustrialScience.proxy.getArmorModel(0);
+        }
         
-        if(armorModel != null){
+        if (armorModel != null) {
             armorModel.bipedHead.showModel = armorSlot == 0;
             armorModel.bipedHeadwear.showModel = armorSlot == 0;
             armorModel.bipedBody.showModel = armorSlot == 1 || armorSlot == 2;
             armorModel.bipedRightArm.showModel = armorSlot == 1;
             armorModel.bipedLeftArm.showModel = armorSlot == 1;
-            armorModel.bipedRightLeg.showModel = armorSlot == 2 || armorSlot == 3;
-            armorModel.bipedLeftLeg.showModel = armorSlot == 2 || armorSlot == 3;
-
+            armorModel.bipedRightLeg.showModel = armorSlot == 2
+                    || armorSlot == 3;
+            armorModel.bipedLeftLeg.showModel = armorSlot == 2
+                    || armorSlot == 3;
+            
             armorModel.isSneak = entityLiving.isSneaking();
             armorModel.isRiding = entityLiving.isRiding();
             armorModel.isChild = entityLiving.isChild();
-            armorModel.heldItemRight = entityLiving.getEquipmentInSlot(0) != null ? 1 :0;
-            if(entityLiving instanceof EntityPlayer){
-            armorModel.aimedBow =((EntityPlayer)entityLiving).getItemInUseDuration() > 2;
+            armorModel.heldItemRight = entityLiving.getEquipmentInSlot(0) != null ? 1
+                    : 0;
+            if (entityLiving instanceof EntityPlayer) {
+                armorModel.aimedBow = ((EntityPlayer) entityLiving)
+                        .getItemInUseDuration() > 2;
             }
             return armorModel;
-            }
-            
-
-            return null;
-            }
-            
-    
+        }
+        
+        return null;
     }
     
-    
-
+}
