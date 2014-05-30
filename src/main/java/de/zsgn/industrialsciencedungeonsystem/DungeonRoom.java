8bb102ab68705bcd1d62a21166795fdb71208c20
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-05-30 21:38:47 +0200

    Added the first classes for the dungeon system.

---
+++ src/main/java/de/zsgn/industrialsciencedungeonsystem/DungeonRoom.java
@@ -0,0 +1,27 @@
+package de.zsgn.industrialsciencedungeonsystem;
+
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+
+public class DungeonRoom {
+public final static int ROOMSIZE=16;
+public final static int ROOMHEIGHT=10;
+private String[][][] content=new String[ROOMSIZE-2][ROOMHEIGHT-2][ROOMSIZE-2];
+private RouteType routetype;
+public DungeonRoom(RouteType routetype, String[][][] content) {
+    super();
+    this.routetype = routetype;
+    this.content = content;
+}
+public static void generateRoom(int x, int y, int z, World worldobj, ForgeDirection direction){
+}
+public String[][][] getContent() {
+    return content;
+}
+public RouteType getRoutetype() {
+    return routetype;
+}
+public void setRoutetype(RouteType routetype) {
+    this.routetype = routetype;
+}
+}
---
+++ src/main/java/de/zsgn/industrialsciencedungeonsystem/RouteType.java
@@ -0,0 +1,6 @@
+package de.zsgn.industrialsciencedungeonsystem;
+
+public enum RouteType {
+    STRAIGHT
+
+}
