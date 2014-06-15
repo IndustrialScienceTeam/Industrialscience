748c93274a79e4c745ed4eb7cab4c6d4457873af
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-06-15 09:58:58 +0200

    Added more needed functions
    like getters and the real fields, still need to save them in NBT Tags

--- src/main/java/de/zsgn/industrialscience/tileentity/TileEntityMultiBlock.java
+++ src/main/java/de/zsgn/industrialscience/tileentity/TileEntityMultiBlock.java
@@ -1,16 +1,42 @@
 package de.zsgn.industrialscience.tileentity;
 
+import de.zsgn.industrialscience.tileentity.multiblock.TileEntityMultiBlockController;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityMultiBlock extends TileEntity {
-
-    public boolean isActivePart() {
-        return false;
-    }
+    protected boolean activepart=false;
+    protected int masterx,mastery,masterz;
 
     public void setController(int x, int y, int z) {
-        // TODO Auto-generated method stub
+        masterx=x;
+        mastery=y;
+        masterz=z;
+    }
+
+    public void destroyStructure() {
+        if(worldObj.getTileEntity(masterx,mastery,masterz) instanceof TileEntityMultiBlockController){
+        ((TileEntityMultiBlockController)worldObj.getTileEntity(masterx,mastery,masterz)).destroyStructure();;
+        }
         
+    }
+
+    public void setActivepart(boolean activepart) {
+        this.activepart = activepart;
+    }
+    public boolean isActivePart() {
+        return activepart;
+    }
+
+    public int getMasterx() {
+        return masterx;
+    }
+
+    public int getMastery() {
+        return mastery;
+    }
+
+    public int getMasterz() {
+        return masterz;
     }
 
 }
