4af8b34fa99353df4c564d6551d6076bb3fc59c5
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-06-15 09:24:19 +0200

    Added a class for the multi block controller

---
+++ src/main/java/de/zsgn/industrialscience/tileentity/multiblock/TileEntityMultiBlockController.java
@@ -0,0 +1,12 @@
+package de.zsgn.industrialscience.tileentity.multiblock;
+
+import de.zsgn.industrialscience.tileentity.TileEntityMultiBlock;
+
+public abstract class TileEntityMultiBlockController extends TileEntityMultiBlock {
+
+    public void setStructureIntact(boolean b) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
