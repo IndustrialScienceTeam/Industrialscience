0104902a0b5b1124ce49a5c2fb28a04abea3963e
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-06-15 09:24:49 +0200

    Added the block class for the controller blocks

---
+++ src/main/java/de/zsgn/industrialscience/block/BlockMultiBlockController.java
@@ -0,0 +1,60 @@
+package de.zsgn.industrialscience.block;
+
+import de.zsgn.industrialscience.MultiBlockStructure;
+import de.zsgn.industrialscience.block.multiblock.IMultiBlockHull;
+import de.zsgn.industrialscience.tileentity.TileEntityMultiBlock;
+import de.zsgn.industrialscience.tileentity.multiblock.TileEntityMultiBlockController;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+
+public abstract class BlockMultiBlockController extends BlockContainer {
+    protected MultiBlockStructure structure;
+    protected Block[] ValidBlocks={};
+    protected BlockMultiBlockController(Material p_i45386_1_, MultiBlockStructure structure) {
+        super(p_i45386_1_);
+        this.structure=structure;
+    } 
+    @Override
+    public boolean onBlockActivated(World world, int x,
+            int y, int z, EntityPlayer player,
+            int side, float xOffset, float yOffset,
+            float zOffset) {
+        if(!world.isRemote && world.getTileEntity(x, y, z) instanceof TileEntityMultiBlockController){
+            TileEntityMultiBlockController masterTileEntity = (TileEntityMultiBlockController) world.getTileEntity(x, y, z);
+            Vec3[] blocks = structure.structureTest(world, x, y, z, ForgeDirection.getOrientation(world.getBlockMetadata(x, y, z)), ValidBlocks);
+            if(blocks == null){
+                return false;
+            }
+            System.out.println("LOLOL");
+            for (int i = 0; i < blocks.length; i++) {
+                Vec3 blockcord = blocks[i];
+                Block block = world.getBlock((int)blockcord.xCoord, (int)blockcord.yCoord, (int)blockcord.zCoord);
+                if(block instanceof IMultiBlockHull){
+                    IMultiBlockHull tileEntityProvider = (IMultiBlockHull) block;
+                    TileEntityMultiBlock tileentity = tileEntityProvider.createNewTileEntity(world, world.getBlockMetadata((int)blockcord.xCoord, (int)blockcord.yCoord, (int)blockcord.zCoord));
+                    tileentity.setController(x,y,z);
+                    world.setTileEntity((int)blockcord.xCoord, (int)blockcord.yCoord, (int)blockcord.zCoord, tileentity);
+                }
+            }
+            masterTileEntity.setStructureIntact(true);
+        }
+        return false;
+    }
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLiving par5EntityLiving, ItemStack par6ItemStack)
+    {
+            int l = MathHelper.floor_double((double)(par5EntityLiving.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
+    }
+    
+
+
+}
