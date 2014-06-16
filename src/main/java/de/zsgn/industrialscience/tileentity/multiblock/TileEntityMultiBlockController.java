16edd83fd12ee309ce2cfc05ee9e3f930345b5b9
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-06-16 11:54:49 +0200

    Added NBT save methods

--- src/main/java/de/zsgn/industrialscience/tileentity/multiblock/TileEntityMultiBlockController.java
+++ src/main/java/de/zsgn/industrialscience/tileentity/multiblock/TileEntityMultiBlockController.java
@@ -1,5 +1,6 @@
 package de.zsgn.industrialscience.tileentity.multiblock;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Vec3;
 
@@ -21,9 +22,43 @@ public abstract class TileEntityMultiBlockController extends TileEntityMultiBloc
         TileEntity tileEntity= worldObj.getTileEntity((int)Coord.xCoord, (int)Coord.yCoord, (int)Coord.zCoord);
         if(tileEntity instanceof TileEntityMultiBlock){
             ((TileEntityMultiBlock)tileEntity).setActivepart(false);
-        }
-            
+        }   
        }
+       structure=null;
+    }
+    @Override
+    public void readFromNBT(NBTTagCompound tagCompound) {
+        super.readFromNBT(tagCompound);
+        NBTTagCompound structurecompound = tagCompound.getCompoundTag("structuretag");
+        if(structurecompound!=null){
+           structure = new Vec3[structurecompound.getInteger("length")];
+           for (int i = 0; i < structure.length; i++) {
+               structure[i].xCoord=structurecompound.getInteger("x"+i);
+               structure[i].yCoord=structurecompound.getInteger("y"+i);
+               structure[i].zCoord=structurecompound.getInteger("z"+i);
+           }
+           else {
+            structurecompound = new Vec3{};
+        }
+        }
+    }
+    @Override
+    public void writeToNBT(NBTTagCompound tagCompound) {
+        super.writeToNBT(tagCompound);
+        if(structure!=null){
+            NBTTagCompound structurecompound = new NBTTagCompound();
+            structurecompound.setInteger("length", structure.length);
+            for (int i = 0; i < structure.length; i++) {
+                Vec3 vec3 = structure[i];
+                structurecompound.setInteger("x"+i, (int)vec3.xCoord);
+                structurecompound.setInteger("y"+i, (int)vec3.yCoord);
+                structurecompound.setInteger("z"+i, (int)vec3.zCoord);
+            }
+            tagCompound.setTag("structuretag", structurecompound);
+        }
+        else {
+            tagCompound.setTag("structuretag", null);
+        }
     }
 
 }
