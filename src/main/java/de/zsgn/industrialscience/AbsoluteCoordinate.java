73faac073d4e5e147182f5b3093d41d791415bcd
Lukas Jansen <lukas.jansen1997@yahoo.de>
2014-06-18 07:52:26 +0200

    Added equals and toString

--- src/main/java/de/zsgn/industrialscience/AbsoluteCoordinate.java
+++ src/main/java/de/zsgn/industrialscience/AbsoluteCoordinate.java
@@ -9,5 +9,17 @@ public AbsoluteCoordinate(int xCoord, int yCoord, int zCoord) {
     this.yCoord = yCoord;
     this.zCoord = zCoord;
 }
+@Override
+public boolean equals(Object obj) {
+    if(obj instanceof AbsoluteCoordinate){
+        AbsoluteCoordinate coord =(AbsoluteCoordinate) obj;
+        return (this.xCoord==coord.xCoord&&this.yCoord==coord.yCoord&&this.zCoord==coord.zCoord);
+    }
+    return false;
+}
+@Override
+public String toString() {
+    return "X: "+xCoord+" - Y: "+yCoord+" - Z: "+zCoord;
+}
 
 }
